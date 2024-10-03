a3cadaa44177fa4dcdcc5cf530fd4483f98bf704
MUKILTEO\1721537 <1721537@mukilteo.wednet.edu>
2024-10-03 10:12:11 -0700

    Taking notes on variables

---
+++ Code Archive/Notes/Notes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes
+{
+    internal class Notes
+    {
+        static void Main(string[] args)
+        {
+            //numerical data types
+            float decimalPoints = 5.7f; //4 bytes
+            int nameOfVariable = 3, fullNumbers = 4; //4 bytes
+            double alsoDecimalPoints = 2, butTwoTimesMemory = 4.6; //8 bytes
+
+            //text data types
+            string anyWords = "Can be anything";
+            char singleCharacterOrLetter = 'g';
+
+            //boolean data types
+            bool trueOrFalse = false;
+
+
+            //changing the variables
+            nameOfVariable = 7;
+            anyWords = "that you like";
+            butTwoTimesMemory = 5.8;
+
+            //read variable value
+            Console.WriteLine(singleCharacterOrLetter);
+
+            //will read the last update so "that you like" not "Can be anything"
+            Console.WriteLine(anyWords);
+        }
+    }
+}
